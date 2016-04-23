1625aa80e55920e96a348bd42ca4479ac8da04f0
luchanso <oleg@loutchansky.com>
2016-04-23 14:51:28 +0300

    Add ads.hx

--- vk/Ads.hx
+++ vk/Ads.hx
@@ -1,10 +1,11 @@
 package;
+import flash.events.Event;
 import openfl.display.Loader;
 import openfl.display.Sprite;
 
-enum AdsType {
-	Preroll;
-	Banner;
+enum PositionType {
+	HORIZONTAL;
+	VERTICAL;
 }
 
 /**
@@ -13,16 +14,49 @@ enum AdsType {
  */
 class Ads
 {
-	static inline var adsFlashUrl:String = "http://api.vk.com/swf/vk_ads.swf";
-	
-	var adsOverlay : Sprite;
-	var adsLoader : Loader;
-
-	public function new(type : AdsType = AdsType.Banner)
-	{
-		adsOverlay = new Sprite();
-		adsLoader = new Loader();
+	public static function create(id : String, x : Float, y : Float, width : Int, height : Int, flashVars : Dynamic, count : Int = 4, isTest : Bool = true, typePosition : PositionType = PositionType.HORIZONTAL)
+	{	
+		var mainVKBanner = new com.vk.MainVKBanner(id);
 		
+		var params = new com.vk.vo.BannersPanelVO(); // создание класса параметров баннера
+		// изменение стандартных параметров:
+		if (isTest) 
+		{
+			params.demo = '1'; // показывает тестовые баннеры
+		} 
+		else 
+		{
+			params.demo = '0'; 
+		}
+		
+		// вертикальный (AD_TYPE_VERTICAL) или горизонтальный (AD_TYPE_HORIZONTAL) блок баннеров
+		
+		if (typePosition == PositionType.HORIZONTAL) 
+		{
+			params.ad_type = com.vk.vo.BannersPanelVO.AD_TYPE_HORIZONTAL;
+			params.ad_unit_type = com.vk.vo.BannersPanelVO.AD_UNIT_TYPE_HORIZONTAL;
+		} 
+		else 
+		{
+			params.ad_type = com.vk.vo.BannersPanelVO.AD_TYPE_VERTICAL;
+			params.ad_unit_type = com.vk.vo.BannersPanelVO.AD_UNIT_TYPE_VERTICAL;
+		}
+		
+		params.ad_height = height;
+		params.ad_width = width;
+		
+		params.ads_count = count;
+		
+		mainVKBanner.addEventListener(com.vk.MainVKBannerEvent.LOAD_COMPLETE, banner_onLoad);
+		mainVKBanner.addEventListener(com.vk.MainVKBannerEvent.LOAD_IS_EMPTY, banner_onAdsEmpty);
+		mainVKBanner.addEventListener(com.vk.MainVKBannerEvent.LOAD_ERROR, banner_onError);
+		
+		mainVKBanner.initBanner(flashVars, params);
+
+		mainVKBanner.x = x;
+		mainVKBanner.y = y;
+		
+		return mainVKBanner;
 	}
 	
 }
